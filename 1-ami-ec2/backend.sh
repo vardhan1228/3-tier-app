3b8a8e14f97d07b14bbbb7a461bb5ddc12574074
srivardhan <159407504+vardhan1228@users.noreply.github.com>
2024-12-18 09:36:13 +0530

    Update backend.sh

--- 1-ami-ec2/backend.sh
+++ 1-ami-ec2/backend.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
 sudo apt update -y
+
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs -y
-sudo apt update -y
-sudo npm install -g corepack -y
-corepack enable
-corepack prepare yarn@stable --activate
-sudo npm install -g pm2
 
+sudo apt update -y
+
+sudo npm install -g corepack -y
+
+corepack enable
+
+corepack prepare yarn@stable --activate 
+
+sudo npm install -g pm2
