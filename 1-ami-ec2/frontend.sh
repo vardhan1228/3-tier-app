b4461f96bef8eeec56264c318cafbc7a039a461f
srivardhan <159407504+vardhan1228@users.noreply.github.com>
2024-12-18 09:35:43 +0530

    Update frontend.sh

--- 1-ami-ec2/frontend.sh
+++ 1-ami-ec2/frontend.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+
 sudo apt update -y
+
 sudo apt install apache2 -y
+
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs -y
+
 sudo apt update -y
-sudo npm install -g corepack -y
+
+sudo npm install -g corepack -y     
+
 corepack enable
-corepack prepare yarn@stable --activate
+
+corepack prepare yarn@stable --activate 
+
 sudo npm install -g pm2
+
